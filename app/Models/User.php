0b462cde5b250a92813d0350ae72e94f9076bfef
David wanjohi <davidwagura83@gmail.com>
2024-03-23 13:07:03 +0300

    create models

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function bike()
+    {
+        return $this->hasone('App\Models\Bikes');
+    }
 }
