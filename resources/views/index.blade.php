9c1e851b90c09d11fed7d7a08474d8aa9fcef7b7
David wanjohi <davidwagura83@gmail.com>
2024-03-23 14:12:04 +0300

    add a view file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+</head>
+<style>
+ 
+ 
+</style>
+<body>
+
+<h1> One to One relationship In Laravel </h1>
+
+<h2>
+
+<p> Names from Users Table </p>
+
+@foreach ($users as $user)
+<li> 
+    
+    {{ $user->name}}  
+ 
+</li>
+@endforeach
+
+<p> Bike Names from Bike Table </p>
+
+@foreach ($allbikes as $allbike)
+<li> 
+    
+    {{ $allbike->bike_name}}  
+ 
+</li>
+@endforeach
+
+<p> User_1 ({{ $user_1_name->name }}) has {{ $user_1_bike->bike_name }} </p>
+
+<p> User_3 ({{ $user_2_name->name }}) has {{ $user_3_bike->bike_name }} </p>
+
+
+</body>
+</html>
